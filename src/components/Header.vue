9b960cac02877dfeca59cd5ebaf40af0270872a5
kzw200015 <kzw200015@gmail.com>
2021-07-04 19:54:21 +0800

    add title config

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,33 @@
 <template>
   <el-menu mode="horizontal">
-    <el-menu-item>服务器监控</el-menu-item>
+    <el-menu-item>{{ title }}</el-menu-item>
   </el-menu>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { ElMenu, ElMenuItem } from "element-plus"
+import { backend } from '../axios'
 
 export default defineComponent({
   components: {
     ElMenu,
     ElMenuItem
+  },
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    async loadTitle() {
+      const resp = await backend.get<string>('/title')
+      this.title = resp.data
+    }
+  },
+  async mounted() {
+    await this.loadTitle()
+    document.title = this.title
   }
-
 })
 </script>
